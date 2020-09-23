aa7f1af34636150566189215d09c4aa9712ea1cf
lex-stokes <lexx_stokes@live.com>
2020-09-24 10:09:55 +1200

    databases - location & scrapbooks

--- server/db/seeds/scrapbooks.js
+++ server/db/seeds/scrapbooks.js
@@ -3,4 +3,7 @@ exports.seed = (knex) =>
   knex("scrapbooks")
     .del()
 // Inserts seed entries
-    .then(() => knex("scrapbooks").insert([]));
+    .then(() => knex("scrapbooks").insert([
+      {user_id: 1, species_id: 1, date_spotted:new Date("24/09/2020")},
+      {user_id: 1, species_id: 2, date_spotted:new Date("")},
+    ]));
