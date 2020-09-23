efb38cfacc7e1ab5913e40d4e1a20cb72994351d
student <student@eda-bc-07.localdomain>
2020-09-24 10:57:59 +1200

    add seeds and migrations for birds, minor changes to users table

--- server/db/migrations/20170829044256_users.js
+++ server/db/migrations/20170829044256_users.js
@@ -1,8 +1,9 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id')
-    table.string('username')
-    table.string('hash')
+    table.string('user_name')
+    table.string('user_hash')
+    table.string('user_img')
     table.timestamps(true,true)
   })
 };
---
+++ server/db/migrations/20200924101027_birds.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('birds', table => {
+    table.increments('id')
+    table.string('bird_name')
+    table.string('bird_english_name')
+    table.string('bird_img')
+    table.string('bird_rarity')
+    table.boolean('bird_nocturnal')
+    table.string('bird_tag')
+    table.string('bird_info')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('birds')
+};
