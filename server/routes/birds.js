0667f7dbb01dea5a3c4e277b9138d4e86e740993
lex-stokes <lexx_stokes@live.com>
2020-09-24 11:48:22 +1200

    Alex asked for it

--- server/routes/birds.js
+++ server/routes/birds.js
@@ -1,7 +1,16 @@
-const express = require('express')
+const express = require("express");
 
-const { getAllHabitats } = require('../db/birds')
+const { getAllHabitats } = require("../db/birds");
 
-const router = express.Router()
+const router = express.Router();
 
-module.exports = router
+router.get("/habitats", getHabitats);
+
+function getHabitats(req, res) {
+  return getAllHabitats().then((habitats) => {
+      console.log(habitats, "and anthony is cool")
+    return res.json({ body: habitats });
+  });
+}
+
+module.exports = router;
