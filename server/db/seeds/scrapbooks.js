4eb02615a50332a64762d4f6120845b4b9dc430e
student <student@eda-bc-07.localdomain>
2020-09-25 10:47:27 +1200

    updated seeds

--- server/db/seeds/scrapbooks.js
+++ server/db/seeds/scrapbooks.js
@@ -4,7 +4,12 @@ exports.seed = (knex) =>
     .del()
 // Inserts seed entries
     .then(() => knex("scrapbooks").insert([
-      {user_id: 1, bird_id: 1},
       {user_id: 1, bird_id: 2},
-      {user_id: 2, bird_id: 2}
+      {user_id: 1, bird_id: 2},
+      {user_id: 1, bird_id: 2},
+      {user_id: 1, bird_id: 3},
+      {user_id: 1, bird_id: 3},
+      {user_id: 1, bird_id: 4},
+      {user_id: 1, bird_id: 6},
+      {user_id: 2, bird_id: 1}
     ]));
