5be87db5f678fc947df02f4c813f930f564a2fb0
itsmattsaudners <itsmattsaunders@gmail.com>
2020-09-25 15:32:17 +1200

    merging in from development

--- client/apis/index.js
+++ client/apis/index.js
@@ -1,26 +1,35 @@
-import request from 'superagent'
+import request from "superagent";
 
-import { getEncodedToken } from 'authenticare/client'
+import { getEncodedToken } from "authenticare/client";
 
-const jsonHeader = { Accept: 'application/json' }
-const authHeader = { Authorization: `Bearer ${getEncodedToken()}` }
+const jsonHeader = { Accept: "application/json" };
+const authHeader = { Authorization: `Bearer ${getEncodedToken()}` };
 
-const apiUrl = '/api/v1/birds'
+const apiUrl = "/api/v1/birds";
 
 // The following two lines need to be set immediately after the request (get/post/patch/delete) to access secure routes
 // .set(jsonHeader)
 // .set(authHeader)
 
-export function demoAPIFunction () {
+export function demoAPIFunction() {
   return request
     .get(apiUrl)
     .set(jsonHeader)
     .set(authHeader)
-    .then(res => res)
-    .catch(errorHandler)
+    .then((res) => res)
+    .catch(errorHandler);
 }
 
 // Global error handler for front end api's
-function errorHandler (err) {
-  console.error(err)
+function errorHandler(err) {
+  console.error(err);
+}
+
+export function getBirds() {
+  return request
+    .get(apiUrl + "/birdTypes")
+    .set(jsonHeader)
+    .set(authHeader)
+    .then((res) => res)
+    .catch(errorHandler);
 }
--- client/components/Scrapbook.jsx
+++ client/components/Scrapbook.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { getBirds } from "../apis/index";
 
 class Scrapbook extends React.Component {
   state = {
     birds: [],
   };
+
+  componentDidMount() {
+    getBirds().then((birds) => {
+      console.log(birds);
+      console.log("birbs");
+    });
+  }
 
   render() {
     return (
@@ -12,7 +20,9 @@ class Scrapbook extends React.Component {
           id="scrapbook"
           className="container content is-full has-background-primary"
         >
-          <h1 className="has-text-white pt-3 has-text-centered">Kereru Go!</h1>
+          <h1 className="has-text-white pt-3 has-text-centered">
+            <i>Kereru Go!</i>
+          </h1>
           <div className="card mx-4">
             <h2 className="has-text-centered pt-4">SCRAPBOOK</h2>
           </div>
