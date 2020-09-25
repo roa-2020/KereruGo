4e3db74ab1bd9b1ef57897bf126d9a8c6216936d
Anthony McGrath <akm@anthonykyle.co.nz>
2020-09-25 16:24:08 +1200

    Update config base URL for production

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 const environments = {
   development: 'https://localhost:8443/api/v1',
   test: 'http://localhost:3333/api/v1',
-  production: '',
+  production: 'https://kererugo.herokuapp.com/api/v1',
   integration: '',
   deployment: '',
   build: ''
