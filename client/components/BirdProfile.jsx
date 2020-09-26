74ecac374f20114328e54afa2db89c62f0c10342
Anthony McGrath <akm@anthonykyle.co.nz>
2020-09-26 19:46:09 +1200

    did it wrong

--- client/components/BirdProfile.jsx
+++ client/components/BirdProfile.jsx
@@ -1,57 +1,68 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import { apiGetBirdById } from "../apis/index"
+import { apiGetBirdById } from "../apis/index";
 // import { receiveScrapbook } from '../actions/scrapbook'
 
-  class BirdProfile extends React.Component {
-    state = {
-      bird: {}
-    }
+class BirdProfile extends React.Component {
+  state = {
+    bird: {},
+  };
 
-   componentDidMount() {
-    apiGetBirdById(this.props.match.params.id)
-      .then(bird => {
-        console.log(bird)
+  componentDidMount() {
+    apiGetBirdById(this.props.match.params.id).then((bird) => {
+      console.log(bird);
+      // this.props.dispatch(receiveScrapbook(scrapbook))
+      this.setState({
+        bird: bird,
+      });
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.match.params.id !== this.props.match.params.id) {
+      apiGetBirdById(this.props.match.params.id).then((bird) => {
+        console.log(bird);
         // this.props.dispatch(receiveScrapbook(scrapbook))
         this.setState({
-          bird: bird
-        })
-      })
-   }
-
-    render() {
-      const bird = this.state.bird
-        return (
-          <>
-            <div 
-            id="bird-profile"
-            className="container content is-full has-background-primary">
-                <div className="bird-profile-img">
-                    <img src={this.state.bird.birdImg && this.state.bird.birdImg} alt="Image of bird"
-                    ></img>
-                </div>
-                <h3 className="birdName">{bird.birdName && bird.birdName}
-                </h3>
-                <h3 className="birdEnglishName"> {bird.birdEnglishName && bird.birdEnglishName}
-                </h3>
-                <h3 className="birdRarity"> {bird.birdRarity && bird.birdRarity}
-                </h3>
-                <h3 className="birdNocturnal"> {bird.birdNocturnal && bird.birdNocturnal}
-                </h3>
-                <h3 className="birdTag"> {bird.birdTag && bird.birdTag}
-                </h3>
-                <h3 className="birdInfo"> {bird.birdInfo && bird.birdInfo}</h3>
-              </div>
-          </>  
-        )
+          bird: bird,
+        });
+      });
     }
+  }
+
+  render() {
+    const bird = this.state.bird;
+    return (
+      <>
+          <div className="bird-profile-img">
+            <img
+              src={this.state.bird.birdImg && this.state.bird.birdImg}
+              alt="Image of bird"
+            ></img>
+          </div>
+          <h3 className="birdName">{bird.birdName && bird.birdName}</h3>
+          <h3 className="birdEnglishName">
+            {" "}
+            {bird.birdEnglishName && bird.birdEnglishName}
+          </h3>
+          <h3 className="birdRarity"> {bird.birdRarity && bird.birdRarity}</h3>
+          <h3 className="birdNocturnal">
+            {" "}
+            {bird.birdNocturnal && bird.birdNocturnal}
+          </h3>
+          <h3 className="birdTag"> {bird.birdTag && bird.birdTag}</h3>
+          <h3 className="birdInfo"> {bird.birdInfo && bird.birdInfo}</h3>
+      
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (globalState) => {
   return {
     auth: globalState.auth,
-    scrapbook: globalState.scrapbook
-}
-}
-export default connect(mapStateToProps)(BirdProfile)
+    scrapbook: globalState.scrapbook,
+  };
+};
+export default connect(mapStateToProps)(BirdProfile);
