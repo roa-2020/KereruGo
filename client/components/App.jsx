2b4f93ca843706972e4e1ab23d6a420748f8c780
itsmattsaudners <itsmattsaunders@gmail.com>
2020-09-26 17:14:36 +1200

    scrapbook is done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,51 +1,51 @@
-import React from 'react'
+import React from "react";
 import "bulma/css/bulma.css";
-import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom'
-import {connect} from 'react-redux'
-import Nav from './Nav'
-import Home from './Home'
-import Login from './Login'
-import Register from './Register'
-import BirdProfile from './BirdProfile'
-import Map from './Map'
-import Scrapbook from './Scrapbook'
+import { HashRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import Nav from "./Nav";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import BirdProfile from "./BirdProfile";
+import Map from "./Map";
+import Scrapbook from "./Scrapbook";
 
-import { checkAuth } from '../actions/auth'
+import { checkAuth } from "../actions/auth";
 
 export class App extends React.Component {
   componentDidMount() {
-    const confirmSuccess = () => { }
-    this.props.dispatch(checkAuth(confirmSuccess))
+    const confirmSuccess = () => {};
+    this.props.dispatch(checkAuth(confirmSuccess));
   }
 
   render() {
-    const { auth } = this.props
+    const { auth } = this.props;
     return (
       <>
         <Router>
           <div className="bodyContent">
             {auth.isAuthenticated ? (
-              <Redirect to = '/map'/>
+              <Redirect to="/map" />
             ) : (
-              <Redirect to = '/' />
+              <Redirect to="/" />
             )}
             <Route exact path="/" component={Home} />
             <Route path="/map" component={Map} />
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
-            {/* <Route path="/birdprofile" component={BirdProfile} /> */}
+            <Route path="/birdprofile" component={BirdProfile} />
             <Route path="/scrapbook" component={Scrapbook} />
-          </div>  
+          </div>
         </Router>
       </>
-    )
+    );
   }
 }
 
-const mapStateToProps = ({auth}) => {
+const mapStateToProps = ({ auth }) => {
   return {
-    auth
-  }
-}
+    auth,
+  };
+};
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
