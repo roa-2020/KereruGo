8da629fe05f9c7031f0463c746ec8f2a56fd251f
Anthony McGrath <akm@anthonykyle.co.nz>
2020-09-28 16:07:57 +1300

    Increase Precision

--- server/db/migrations/20200924101502_locations.js
+++ server/db/migrations/20200924101502_locations.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('locations', table => {
       table.increments('id').primary()
-      table.decimal('latitude', 10,8)
-      table.decimal('longitude', 10,8)
+      table.decimal('latitude', 12,8)
+      table.decimal('longitude', 12,8)
   })
 };
 
