ab20d4cdda2b9007a6cebe0d940dc28b74dc6658
Anthony McGrath <akm@anthonykyle.co.nz>
2020-09-28 15:47:38 +1300

    Fix scale

--- server/db/migrations/20200924101502_locations.js
+++ server/db/migrations/20200924101502_locations.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('locations', table => {
       table.increments('id').primary()
-      table.decimal('latitude', 8, 10)
-      table.decimal('longitude', 8, 10)
+      table.decimal('latitude', 10,8)
+      table.decimal('longitude', 10,8)
   })
 };
 
